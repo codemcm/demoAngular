26e9fd215599dfbccbe46fbce61d1d70ea0ad303
Martin Cruz M <codemcm@hotmail.com>
2021-01-24 16:41:58 -0600

    components add

--- prototipo/src/app/app.module.ts
+++ prototipo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarEditarComentarioComponent } from './componentes/agregar-editar-comentario/agregar-editar-comentario.component';
+import { ListComentariosComponent } from './componentes/list-comentarios/list-comentarios.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { VerComentarioComponent } from './componentes/ver-comentario/ver-comentario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEditarComentarioComponent,
+    ListComentariosComponent,
+    NavBarComponent,
+    VerComentarioComponent
   ],
   imports: [
     BrowserModule,
